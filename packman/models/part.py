ae0f24157b835150b662e44f94a9abff9bf04f31
Kiran Jonnalagadda <kiran@hasgeek.com>
2014-10-06 19:18:26 +0530

    Added Part.compatible_with

--- packman/models/part.py
+++ packman/models/part.py
@@ -9,6 +9,7 @@ __all__ = ['PartInterface', 'Part', 'PartInstance']
 class PartInterface(TimestampMixin, db.Model):
     __tablename__ = 'part_interface'
     part_id = db.Column(None, db.ForeignKey('part.id'), primary_key=True)
+    # PartInterface.part is defined in the backref from Part
     interface_id = db.Column(None, db.ForeignKey('interface.id'), primary_key=True)
     interface = db.relationship(ConnectionInterface, backref=db.backref('interface_parts', cascade='all, delete-orphan'))
     count = db.Column(db.Integer, nullable=False, default=1)
@@ -20,6 +21,21 @@ class Part(NodeMixin, Node):
     description = db.Column(db.UnicodeText)
     part_interfaces = db.relationship(PartInterface, cascade='all, delete-orphan', backref='part')
     #: TODO: Other metadata such as price
+
+    def compatible_with(self, part):
+        """
+        Do we have an interface that is coupled with one of the interfaces on the other part?
+        """
+        if isinstance(part, Part):
+            # TODO: Delegate this to the database with an SQL join query
+            local_interfaces = [pi.interface for pi in self.part_interfaces]
+            remote_interfaces = [pi.interface for pi in part.part_interfaces]
+
+            for li in local_interfaces:
+                for ri in remote_interfaces:
+                    if li in ri.couples:
+                        return True
+        return False
 
 
 class PartInstance(NodeMixin, Node):

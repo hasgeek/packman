82ed69f78faedf233ccc122e498b59b601014ac5
Kiran Jonnalagadda <kiran@hasgeek.com>
2014-10-09 15:23:28 +0530

    Make a bundle spec

--- packman/models/bundle.py
+++ packman/models/bundle.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import db, NodeMixin, Node, make_timestamp_columns
-from .part import Part
+from .part import Part, PartInstance
 
 __all__ = ['BundleSpec', 'Bundle']
 
@@ -30,3 +30,22 @@ class Bundle(NodeMixin, Node):
     description = db.Column(db.UnicodeText)
     bundlespec_id = db.Column(None, db.ForeignKey('bundlespec.id'), nullable=True)
     bundlespec = db.relationship(BundleSpec, foreign_keys=[bundlespec_id], backref='bundles')
+
+    def make_spec(self, replace=False):
+        """
+        Convert this bundle into a spec.
+        """
+        if self.bundlespec and replace == False:
+            bundlespec = self.bundlespec
+        else:
+            bundlespec = BundleSpec(title=self.title, parent=self.parent)
+            self.bundlespec = bundlespec
+
+        def add_nodes(node):
+            for item in node.nodes:
+                if isinstance(item, PartInstance):
+                    if item.part not in bundlespec.parts:
+                        bundlespec.parts.append(item.part)
+                add_nodes(item)  # Recurse through sub-nodes looking for part instances
+        add_nodes(self)
+        return bundlespec
